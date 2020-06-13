e430cf3af4ea267759bd1181efad30de4fc06104
Twel Tar <sulatttweltarbo16@gmail.com>
2020-06-14 03:17:09 +0630

    commit

--- src/main/java/com/tweltar/conferenceapp/config/PersistenceConfiguration.java
+++ src/main/java/com/tweltar/conferenceapp/config/PersistenceConfiguration.java
@@ -8,11 +8,11 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PersistenceConfiguration {
-	@Bean
-	public DataSource dataSource() {
-		DataSourceBuilder builder = DataSourceBuilder.create();
-		builder.url("jdbc:mysql://localhost:3306/conference_app");
-		System.out.println("My custom datasource bean has been initialized and set");
-		return builder.build();
-	}
+//	@Bean
+//	public DataSource dataSource() {
+//		DataSourceBuilder builder = DataSourceBuilder.create();
+//		builder.url("jdbc:mysql://localhost:3306/conference_app");
+//		System.out.println("My custom datasource bean has been initialized and set");
+//		return builder.build();
+//	}
 }
